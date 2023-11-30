ff4b3dfdf9bc700dbeec8202078d6871c9053a93
Danilburov <499732@student.fontys.nl>
2023-11-30 09:14:55 +0100

    test

---
+++ TicTacToe/src/main/java/main/tictactoe/Application.java
@@ -0,0 +1,22 @@
+package main.tictactoe;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TicTacToe.fxml"));
+        Scene scene = new Scene(loader.load(), 100, 100);
+        stage.setTitle("TicTacToe!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
