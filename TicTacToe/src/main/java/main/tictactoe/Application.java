51464e51ce0c5e8ff08f187b97b2a84a05bb522c
Danilburov <499732@student.fontys.nl>
2023-12-06 22:13:01 +0100

    small change 2

--- TicTacToe/src/main/java/main/tictactoe/Application.java
+++ TicTacToe/src/main/java/main/tictactoe/Application.java
@@ -9,13 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere".fxml"));
-=======
->>>>>>> parent of f6ae692 (changeApp)
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere".fxml));
         Scene scene = new Scene(loader.load(), 100, 100);
         stage.setTitle("TicTacToe!");
         stage.setScene(scene);
