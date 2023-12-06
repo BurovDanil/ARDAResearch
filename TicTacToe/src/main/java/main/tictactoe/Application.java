8f8e4d61906cc0b16b7e85fb63d5cf7c14c22066
GiulioRaffaeli <117159633+GiulioRaffaeli@users.noreply.github.com>
2023-12-06 19:40:59 +0100

    Revert "submit 2"
    This reverts commit f8cba30a7a36c75d8befa90c5415ddc34b3ce421.

--- TicTacToe/src/main/java/main/tictactoe/Application.java
+++ TicTacToe/src/main/java/main/tictactoe/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("TicTacToe.fxml"));
         Scene scene = new Scene(loader.load(), 500, 500);
-        stage.setTitle("I was there");
+        stage.setTitle("TicTacToe!");
         stage.setScene(scene);
         stage.show();
     }
