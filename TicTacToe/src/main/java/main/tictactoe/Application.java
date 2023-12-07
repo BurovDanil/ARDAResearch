60d10af557c586ff065165f1e1175260130c989d
Danilburov <499732@student.fontys.nl>
2023-12-07 16:04:33 +0100

    Revert "submit 3"
    This reverts commit 6e452dbed17f8cc4af61842caa627132737f8864.

--- TicTacToe/src/main/java/main/tictactoe/Application.java
+++ TicTacToe/src/main/java/main/tictactoe/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloTher".fxml));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere".fxml));
         Scene scene = new Scene(loader.load(), 100, 100);
         stage.setTitle("TicTacToe!");
         stage.setScene(scene);
