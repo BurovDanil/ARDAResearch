54793b3668366546a1adfdf0d72883515e901fbc
Danilburov <499732@student.fontys.nl>
2023-12-07 10:31:52 +0100

    Revert "submit 1"
    This reverts commit b0de3eb448d576cd8a5965e86e2aa4238606ccbb.

--- TicTacToe/src/main/java/main/tictactoe/Application.java
+++ TicTacToe/src/main/java/main/tictactoe/Application.java
@@ -9,12 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< Updated upstream
         FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere".fxml));
-=======
-<<<<<<< HEAD
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("HelloThere".fxml"));
->>>>>>> Stashed changes
         Scene scene = new Scene(loader.load(), 100, 100);
         stage.setTitle("TicTacToe!");
         stage.setScene(scene);
